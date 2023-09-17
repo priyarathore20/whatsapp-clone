200e8e5061f25b393262318751df5b65a5c2f10f
aman goyal <amangoyal1198@hotmail.com>
2023-09-17 16:47:55 +0530

    login page design

--- src/Pages/LandingPage/LandingPage.styled.js
+++ src/Pages/LandingPage/LandingPage.styled.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components/macro'
+import styled from "styled-components/macro";
 
 export const LoginPage = styled.div`
   background-color: #111b21;
@@ -6,51 +6,47 @@ export const LoginPage = styled.div`
   height: 100vh;
   position: relative;
 
-.login-navbar {
-  background-color: #00a884;
-  position: absolute;
-  width: 100%;
-  height: 222px;
-}
+  .login-navbar {
+    background-color: #00a884;
+    position: absolute;
+    width: 100%;
+    height: 222px;
+  }
 
-img{
-  height: 39px;
-  width: 39px;
-  fill: #00e676;
-  color: white;
-  outline: #fff;
-}
-` 
+  img {
+    height: 39px;
+    width: 39px;
+    fill: #00e676;
+    color: white;
+    outline: #fff;
+  }
+`;
 
-export const LoginBox =styled.div`
+export const LoginBox = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-    position: absolute;
-    width: 100%;
+  position: relative;
+  width: 100%;
+  z-index: 10;
 
-.logo {
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  width: 1000px;
-  margin: 27px 98px 28px;
-}
+  .logo {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    width: 1000px;
+    margin: 27px 98px 28px;
+  }
 
-
-.logo-title {
-  font-size: 18px;
-  color: #fff;
-  font-weight: 500;
-  text-transform: uppercase;
-  margin-left: 14px;
-}
-
-`
+  .logo-title {
+    font-size: 18px;
+    color: #fff;
+    font-weight: 500;
+    text-transform: uppercase;
+    margin-left: 14px;
+  }
+`;
 
 export const LoginWindow = styled.div`
   background-color: #fff;
@@ -63,76 +59,75 @@ export const LoginWindow = styled.div`
   width: 100%;
 
   .login-window-details {
-  padding: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  flex-direction: column;
-  height: 550px;
-}
+    padding: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+    height: 550px;
+  }
 
-.number-title {
-  font-size: 28px;
-  color: #415258;
-}
+  .number-title {
+    font-size: 28px;
+    color: #415258;
+  }
 
-.number-para {
-  font-size: 16px;
-  color: #8696a0;
-}
+  .number-para {
+    font-size: 16px;
+    color: #8696a0;
+  }
 
-.flag-img {
-  height: 20px;
-  width: 20px;
-}
+  .flag-img {
+    height: 20px;
+    width: 20px;
+  }
 
-.flag-name {
-  margin-left: 14px;
-  font-size: 17px;
-  color: #111b21;
-}
+  .flag-name {
+    margin-left: 14px;
+    font-size: 17px;
+    color: #111b21;
+  }
 
-.number-btn {
-  padding: 14px 10px 15px 26px;
-  border: #e9edef 1px solid;
-  background-color: white;
-  width: 291px;
-  height: 51px;
-  border-radius: 4px;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-}
+  .number-btn {
+    padding: 14px 10px 15px 26px;
+    border: #e9edef 1px solid;
+    background-color: white;
+    width: 291px;
+    height: 51px;
+    border-radius: 4px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
 
-.number {
-  padding: 14px 10px 15px 26px;
-  border: #e9edef 1px solid;
-  background-color: white;
-  width: 291px;
-  height: 51px;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  border-radius: 4px;
-}
+  .number {
+    padding: 14px 10px 15px 26px;
+    border: #e9edef 1px solid;
+    background-color: white;
+    width: 291px;
+    height: 51px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    border-radius: 4px;
+  }
 
-.number-input {
-  outline: none;
-  border: none;
-  margin-left: 14px;
-}
+  .number-input {
+    outline: none;
+    border: none;
+    margin-left: 14px;
+  }
 
-.login-btn{
-  padding: 10px 24px;
-  background-color: #008069;
-  color: #fff;
-  font-size: 14px;
-  border: none;
-  border-radius: 3px;
-  margin-bottom: 5px;
-}
-
-`
+  .login-btn {
+    padding: 10px 24px;
+    background-color: #008069;
+    color: #fff;
+    font-size: 14px;
+    border: none;
+    border-radius: 3px;
+    margin-bottom: 5px;
+  }
+`;
 
 export const LoginTutorial = styled.div`
   display: flex;
@@ -141,18 +136,15 @@ export const LoginTutorial = styled.div`
   justify-content: flex-start;
   height: 200px;
 
-  .login-tutorial-heading{
-  color: #415258;
-  font-size: 28px;
-  margin: 0px 0px 16px;
-}
+  .login-tutorial-heading {
+    color: #415258;
+    font-size: 28px;
+    margin: 0px 0px 16px;
+  }
 
-.tutorial-link{
-  text-decoration: none;
-  font-size: 14px;
-  color: #008069;
-}
-`
-
-
-
+  .tutorial-link {
+    text-decoration: none;
+    font-size: 14px;
+    color: #008069;
+  }
+`;
